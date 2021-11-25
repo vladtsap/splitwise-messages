17151e31d63e6475f8caee71b1daef5c49afa661
vladtsap <vldzpp@gmail.com>
2021-11-25 02:35:37 +0200

    add message_view to transaction

--- monobank.py
+++ monobank.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter, Response, Depends
 from sqlalchemy.orm import Session
 
@@ -19,11 +21,33 @@ def get_db():
         db.close()
 
 
+def message_view(transaction: schemas.WebhookTransaction) -> str:
+    result = '🤑 ' if transaction.amount < 0 else '🍀 '
+    result += f'<b>{transaction.amount / 100}'
+    result += ' → ' if transaction.amount < 0 else ' ← '
+    result += f'{transaction.description}</b>\n'
+
+    if transaction.cashback_amount:
+        result += f'💫 кешбек: {transaction.cashback_amount / 100}\n'
+
+    if transaction.commission_rate:
+        result += f'🧨 комісія: {transaction.commission_rate / 100}\n'
+
+    result += f'🏦 залишок: {transaction.balance / 100}\n'
+
+    date = datetime.datetime.fromtimestamp(transaction.time)
+    result += f'🕑 {date.strftime("%d.%m %H:%M")}\n'
+
+    return result
+
+
 @router.post('')
 async def new_transaction(webhook: schemas.Webhook, db: Session = Depends(get_db)):
+    webhook_transaction = webhook.data.statement_item
+
     message = await bot.send_message(
         chat_id=TELEGRAM_USER_ID,
-        text='transaction',
+        text=message_view(webhook_transaction),
         disable_notification=True,
     )
 
